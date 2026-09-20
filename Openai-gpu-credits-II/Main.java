023307161d43b1ea0b5d0295d5fa69e1b0abc7ee
sdshou <shaodishou@gmail.com>
2026-09-20 11:26:46 -0700

    fix solution bug for GPU Credits II

--- Openai-gpu-credits-II/Main.java
+++ Openai-gpu-credits-II/Main.java
@@ -4,67 +4,116 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         CreditSystem system = new CreditSystem();
-        system.grantCredit(3, 10, 60);
+        system.grantCredit("a", 3, 10, 60);
         System.out.println(system.getBalance(10)); // 3
-        system.grantCredit(2, 20, 40);
+        system.grantCredit("b", 2, 20, 40);
         system.subtract(1, 30);
+        system.subtract(1, 40);
         system.subtract(3, 50);
         System.out.println(system.getBalance(10)); // 3
         System.out.println(system.getBalance(20)); // 5
         System.out.println(system.getBalance(30)); // 4
         System.out.println(system.getBalance(35)); // 4
-        System.out.println(system.getBalance(40)); // 3
+        System.out.println(system.getBalance(40)); // 2
         System.out.println(system.getBalance(50)); // 0
         System.out.println(system.getBalance(55)); // 0
     }
 }
 
 class CreditSystem {
-    private Map<Integer, Credit> grants = new TreeMap<>();
-    private Map<Integer, Integer> subs = new TreeMap<>();
+    private Set<Event> events = new TreeSet<>((a, b) -> {
+        if (a.start == b.start) {
+            if (a.type == b.type && a.id != null) {
+                return a.id.compareTo(b.id);
+            }
+            return a.type - b.type;
+        }
+        return a.start - b.start;
+    });
+
+    private Map<Integer, Event> subs = new HashMap<>();
 
     // the 'end' is exclusive
-    public void grantCredit(int credit, int start, int end) {
-        this.grants.put(start, new Credit(credit, start, end));
+    // O(logn)
+    public void grantCredit(String id, int credit, int start, int end) {
+        this.events.add(new Event(id, 0, start, end, credit));
     }
 
+    // O(klogk + klogk + n)
     public int getBalance(int timestamp) {
-        int left = timestamp;
+        // 可能只要放 [end, credit] 即可
         Queue<Credit> pq = new PriorityQueue<>((a, b) -> a.end - b.end);
-        for (Map.Entry<Integer, Credit> grant : grants.entrySet()) {
-            if (grant.getKey() > timestamp) break;
-            Credit c = grant.getValue();
-            if (c.end > timestamp) {
-                pq.offer(new Credit(c.credit, c.start, c.end));
-                left = Math.min(left, c.start);
-            }
-        }
-        for (Map.Entry<Integer, Integer> sub : subs.entrySet()) {
-            var subTime = sub.getKey();
-            if (subTime > timestamp) break;
-            if (subTime >= left) {
-                var subVal = sub.getValue();
-                while (!pq.isEmpty()) {
-                    Credit head = pq.peek();
-                    if (head.start > subTime) continue;
-                    if (head.credit > subVal) {
-                        head.credit -= subVal;
-                        break;
+        // 必须从最左边开始扫，不然会漏掉解（corner case）
+        for (Event event : events) {
+            if (event.start > timestamp) break;
+            switch (event.type) {
+                case 0: {
+                     // O(logk)
+                    pq.offer(new Credit(event.credit, event.start, event.end));
+                    break;
+                }
+                case 1: {
+                    int subval = event.credit;
+                    int subtime = event.start;
+                    while (!pq.isEmpty()) {
+                        Credit cur = pq.peek();
+                        if (cur.end <= subtime) {
+                            pq.poll();
+                            continue;
+                        }
+                        if (cur.credit > subval) {
+                            cur.credit -= subval;
+                            break;
+                        } else {
+                            pq.poll();
+                            subval -= cur.credit;
+                        }
                     }
-                    pq.poll();
-                    subVal -= head.credit;
                 }
             }
         }
+        
         int amount = 0;
         while (!pq.isEmpty()) {
-            amount += pq.poll().credit;
+            Credit c = pq.poll();
+            if (c.end <= timestamp) continue;
+            amount += c.credit;
         }
         return amount;
     }
 
+    // O(logn)
     public void subtract(int credit, int timestamp) {
-        subs.put(timestamp, subs.getOrDefault(timestamp, 0) + credit);
+        if (this.subs.containsKey(timestamp)) {
+            Event event = this.subs.get(timestamp); // O(1)
+            this.events.remove(event); // O(logn)
+            event.credit += credit;
+            this.events.add(event); // O(logn)
+        } else {
+            Event event = new Event(1, timestamp, timestamp, credit);
+            this.subs.put(timestamp, event);
+            this.events.add(event);
+        }
+    }
+}
+
+class Event {
+    int type;// 0: start, 1: end; 2: sub
+    int start;
+    int end;
+    int credit;
+    String id;
+
+    public Event(String id, int type, int start, int end, int credit) {
+        this.id = id;
+        this.type = type;
+        this.start = start;
+        this.end = end;
+        this.credit = credit;
+    }
+
+    public Event(int type, int start, int end, int credit) {
+        this(null, type, start, end, credit);
     }
 }
 
